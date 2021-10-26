b0a54a4144c895badbc50732f5303385206264e5
João Mário <joaomario.s.d@gmail.com>
2021-10-26 16:11:58 -0300

    Calculadora com While

---
+++ CalculadoraSimples/Entities/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace CalculadoraSimples.Entities
+{
+    class Calculadora
+    {
+        public double Val { get; set; }
+        public double Val2 { get; set; }
+
+        public Calculadora()
+        {
+
+        }
+
+        public Calculadora(double val, double val2)
+        {
+            Val = val;
+            Val2 = val2;
+        }
+
+        public double Soma()
+        {
+            Console.Write("Digite o primeiro número: ");
+            Val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o segundo número: ");
+            Val2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            double sum = Val + Val2;
+            return sum;
+        }
+
+        public double Subtracao()
+        {
+            Console.Write("Digite o primeiro número: ");
+            Val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o segundo número: ");
+            Val2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            double sub = Val - Val2;
+            return sub;
+        }
+
+        public double Multiplicacao()
+        {
+            Console.Write("Digite o primeiro número: ");
+            Val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o segundo número: ");
+            Val2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            double mult = Val - Val2;
+            return mult;
+        }
+
+        public double Divisao()
+        {
+            Console.Write("Digite o primeiro número: ");
+            Val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite o segundo número: ");
+            Val2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            double div = Val - Val2;
+            return div;
+
+        }
+    }
+}
---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using CalculadoraSimples.Entities;
+
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Calculadora!".ToUpper());
+            Console.WriteLine();
+
+            bool condicao = true;
+            while (condicao)
+            {
+                Console.WriteLine("Digite a operação que deseja fazer:");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("+ = Soma, - = Subtração, * = Multiplicação, / = Divisão, s = Sair: ");
+                string c = Console.ReadLine();
+                Console.WriteLine();
+
+                Calculadora calculadora = new Calculadora();           
+
+                switch (c)
+                {
+                    case "+":
+                        Console.WriteLine("O resultado da soma de " + calculadora.Soma().ToString("F2", CultureInfo.InvariantCulture));
+                        Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+                    case "-":
+                        Console.WriteLine("O resultado da subtração de " + calculadora.Subtracao().ToString("F2", CultureInfo.InvariantCulture));
+                        Console.ReadLine();
+                        Console.WriteLine();
+
+                        break;
+                    case "*":
+                        Console.WriteLine("O resultado da multiplicação de " + calculadora.Multiplicacao().ToString("F2", CultureInfo.InvariantCulture));
+                        Console.ReadLine();
+                        Console.WriteLine();
+
+                        break;
+                    case "/":
+                        Console.WriteLine("O resultado da divisão de " + calculadora.Divisao().ToString("F2", CultureInfo.InvariantCulture));
+                        Console.ReadLine();
+                        Console.WriteLine();
+
+                        break;
+                    case "s":
+                        condicao = false;
+                        Console.WriteLine("Saindo!");
+                        Console.ReadLine();
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Nenhuma das opções disponíveis foi escolhidas!");
+                        break;
+                }
+
+                Console.Clear();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadKey();
+        }
+    }
+}
